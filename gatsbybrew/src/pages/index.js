60f73bae6ec485b0bb8f345daf50dfbc0acd9914
Aleksey Kozin <cirnotoss@gmail.com>
2020-02-27 02:14:20 +0300

    test

--- gatsbybrew/src/pages/index.js
+++ gatsbybrew/src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
-    <p>API: {process.env.GATSBY_REST_API}</p>
+    <p>API_: {process.env.GATSBY_REST_API}</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
